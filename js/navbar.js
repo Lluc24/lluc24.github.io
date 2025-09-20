3eb035da90b592fbb489c6d0d0b968617bde30ff
Lluc Santamaria <lluc>
2025-09-20 14:21:33 +0200

    Added footer + refactored structure

--- js/navbar.js
+++ js/navbar.js
@@ -1,5 +1,5 @@
 // Fetch navbar.html and insert it into the page
-fetch('navbar.html')
+fetch('partial/navbar.html')
     .then(response => response.text()) // get the HTML content
     .then(data => {
         document.getElementById('navbar').innerHTML = data;
